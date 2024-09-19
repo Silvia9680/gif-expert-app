0485d0ec04054ca07fe51d66861025a475bdc3b2
Silvia9680 <silviacarolina9680@gmail.com>
2024-09-19 19:56:55 -0300

    first commit

---
+++ gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,9 @@
+
+
+export const GifExpertApp = () => {
+  return (
+    <>
+        <h1>GiftExpertApp</h1>
+    </>
+  )
+}
---
+++ gif-expert-app/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { GifExpertApp } from './GifExpertApp'
+import './styles.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <GifExpertApp />
+  </StrictMode>,
+)
+
+
+//ApiKey: JoP5onwjmII4I7ph7e999nK9Q5ZfJmYJ
